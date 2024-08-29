6e9e953502963f848a33b0125998dfdb350e3a88
Vijaya Pacnahmurthy <pvijaya@synopsys.com>
2024-08-29 21:59:29 +0530

    added sample go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package backstage_catalog
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", helloWorld)
+	e.Start(":9000")
+}
+
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
